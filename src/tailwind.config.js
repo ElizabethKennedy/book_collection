2a71ecd9a489eecb2f399773e96262e4361271c9
ElizabethKennedy <github.com/ElizabethKennedy>
2024-02-15 15:49:18 -0800

    transferred from old repo

---
+++ src/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.{html,js}"],
+    theme: {
+      extend: {
+        fontFamily: {
+          EB_Garamond: ["EB_Garamond", "serif"],
+        },
+        maxWidth: {
+          inputMax: "15rem",
+        },
+      },
+    },
+    plugins: [],
+  };
